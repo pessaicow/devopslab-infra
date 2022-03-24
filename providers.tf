e06408ad3afd0e7bb43af310cbfd1529f1c1d749
pessaicow <pessaicow@hotmail.com>
2022-03-24 14:57:05 -0300

    Terraform GCP Basics

---
+++ providers.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.14.0"
+    }
+  }
+}
+
+# Configura o Provider Google Cloud com o Projeto
+provider "google" {
+
+  credentials = file("/home/vagrant/gcpkey.json")
+
+  project = "devopslab-345001"
+  region  = "us-west1"
+  zone    = "us-west1-b"
+}
